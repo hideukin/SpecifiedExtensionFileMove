142c9834347d1a6a357b73039e416d0ac1a06a58
hideukin <hideukin@gmail.com>
2017-01-11 10:57:46 +0900

    Initial Commit.

---
+++ SpecifiedExtensionFileMove/SpecifiedExtensionFileMove/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace SpecifiedExtensionFileMove
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// 選択ボタンによるフォルダ選択および画面表示
+        /// </summary>
+        /// <param name="sender">オブジェクト</param>
+        /// <param name="e">イベントハンドラ</param>
+        private async void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+            folderPicker.FileTypeFilter.Add("*");
+            Windows.Storage.StorageFolder folder =
+                await folderPicker.PickSingleFolderAsync();
+
+            if (folder == null)
+            {
+                return;
+            }
+
+            SavedFolderPathLabel.Text = folder.Path.ToString();
+        }
+    }
+}
